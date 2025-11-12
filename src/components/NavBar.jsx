714c31ea35542bdb321102214a3a381056c54a8c
MasadRayan <masadrayan2002@gmail.com>
2025-11-13 04:51:48 +0600

    Navbar sticky done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,8 +14,8 @@ const NavBar = () => {
         </>
     )
     return (
-        <div>
-            <div className="navbar bg-base-100 shadow-sm">
+        <div className='sticky top-0 z-50 bg-transparent backdrop-blur-2xl container mx-auto'>
+            <div className="navbar ">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
