92285c9580b1da4ae1364c8916bb34b8cfa24f4d
MasadRayan <masadrayan2002@gmail.com>
2025-11-17 02:52:13 +0600

    AuthOptions added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,54 +1,5 @@
-import axios from "axios";
+import { authOptions } from "@/lib/authOptions"
 import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-
-export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials, req) {
-                const res = await axios.post("http://localhost:5000/api/user/login", credentials);
-                const user = res.data;
-                console.log("From the user chk", user);
-
-                if (user?.message) {
-                    throw new Error(user.message);
-                }
-
-                if (!user) {
-                    throw new Error("Login failed");
-                }
-
-                return user
-            }
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        })
-    ],
-    pages: {
-        signIn: '/auth-login',
-
-    }
-}
 
 const handler = NextAuth(authOptions)
-
 export { handler as GET, handler as POST }
