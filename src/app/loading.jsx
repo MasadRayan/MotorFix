2895d232f25d8ee32cd03a4f42e11e780230d7bc
MasadRayan <masadrayan2002@gmail.com>
2025-11-18 03:00:28 +0600

    Custom loading added

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const loading = () => {
     return (
-        <div>
-            <h1>Loading...</h1>
+        <div className="flex items-center justify-center h-screen">
+            <span className="loading loading-bars text-[#FF3811] w-22 h-22"></span>
         </div>
     );
 };
