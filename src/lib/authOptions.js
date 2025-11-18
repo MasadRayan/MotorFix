548d1b8102e2835422507b170c7263851690a42c
MasadRayan <masadrayan2002@gmail.com>
2025-11-19 00:36:17 +0600

    Modified in authoptions

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -8,12 +8,7 @@ export const authOptions = {
     // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
+            name: "Credentials", 
             credentials: {
                 username: { label: "Username", type: "text", placeholder: "jsmith" },
                 password: { label: "Password", type: "password" }
