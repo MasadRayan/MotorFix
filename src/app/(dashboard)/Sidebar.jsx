997b845f1039dc071aa10219e8332b1449eb2f8c
MasadRayan <masadrayan2002@gmail.com>
2025-11-19 01:47:16 +0600

    User dashboard sidebar added

--- src/app/(dashboard)/Sidebar.jsx
+++ src/app/(dashboard)/Sidebar.jsx
@@ -1,10 +1,39 @@
+import Link from 'next/link';
 import React from 'react';
 
-const Sidebar = ({role}) => {
+const Sidebar = ({ role }) => {
     // console.log(role);
     return (
         <div>
-            
+            {/* ROLE BASED LINKS */}
+            {role === "admin" ? (
+                <>
+                    <li><Link href="/dashboard/adminDashboard">
+                        <button>
+                            Admin Home
+                        </button>
+                    </Link></li>
+                    <li><Link href="/dashboard/manageBookings">Manage Bookings</Link></li>
+                    <li><Link href="/dashboard/manageUsers">Manage Users</Link></li>
+                </>
+            ) : (
+                <>
+                    <div className='flex flex-col justify-center gap-5 pl-5 w-full '>
+                        <Link href="/dashboard/userDashboard">
+                            <button className='btn btn-outline w-full text-[#FF3811] hover:bg-[#FF3811] hover:text-white'>
+                                User Home
+                            </button>
+                        </Link>
+                        <Link href="/dashboard/userDashboard">
+                            <button className='btn btn-outline w-full text-[#FF3811] hover:bg-[#FF3811] hover:text-white'>
+                                My Bookings
+                            </button>
+                        </Link>
+                        
+                    </div>
+
+                </>
+            )}
         </div>
     );
 };
