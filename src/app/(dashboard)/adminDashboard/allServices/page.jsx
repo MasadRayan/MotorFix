1e5b6a07f05f6ba72ebcadf1cdbc2dd609bd21ef
MasadRayan <masadrayan2002@gmail.com>
2025-11-22 22:46:52 +0600

    All service page design done

--- src/app/(dashboard)/adminDashboard/allServices/page.jsx
+++ src/app/(dashboard)/adminDashboard/allServices/page.jsx
@@ -1,6 +1,16 @@
+import { authOptions } from '@/lib/authOptions';
+import axios from 'axios';
+import { getServerSession } from 'next-auth';
 import React from 'react';
 
-const AllServicesPages = () => {
+const AllServicesPages = async () => {
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email
+    const res = await fetch(`http://localhost:5000/api/service/admin/${email}`, {
+        next: { revalidate: 0 }, 
+    });
+    const data = await res.json();
+    // console.log(data);
     return (
         <div>
             
---
+++ src/app/(dashboard)/adminDashboard/page.jsx
@@ -0,0 +1,12 @@
+import { getServerSession } from 'next-auth';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default page;
